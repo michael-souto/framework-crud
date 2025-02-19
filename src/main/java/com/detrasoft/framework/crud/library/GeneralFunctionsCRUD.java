dad2777a174697b79c0f24c9b2d4f4fb2f154b3b
Michael Souto <michaelsouto@outlook.com>
2025-02-19 11:56:49 -0300

    Update GeneralFunctionsCRUD.java

--- src/main/java/com/detrasoft/framework/crud/library/GeneralFunctionsCRUD.java
+++ src/main/java/com/detrasoft/framework/crud/library/GeneralFunctionsCRUD.java
@@ -1,6 +1,7 @@
 package com.detrasoft.framework.crud.library;
 
 import com.detrasoft.framework.crud.entities.GenericEntity;
+import com.detrasoft.framework.crud.services.crud.GenericCRUDService;
 import org.hibernate.collection.spi.PersistentBag;
 
 import jakarta.persistence.ManyToMany;
@@ -8,6 +9,13 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import org.springframework.beans.BeanUtils;
 
 public class GeneralFunctionsCRUD {
     public static boolean checkEmptyOrNull(GenericEntity entity) {
@@ -67,5 +75,55 @@ public class GeneralFunctionsCRUD {
         } catch (IllegalArgumentException | IllegalAccessException e) {
             e.printStackTrace();
         }
+    }
+
+    public static <T extends GenericEntity> void mergeLists(
+            List<T> source,
+            List<T> target,
+            GenericCRUDService<T> service,
+            BiConsumer<T, T> copyProperties,
+            Consumer<T> newEntityInitializer) {
+
+        Map<UUID, T> targetMap = target.stream()
+                .filter(t -> t.getId() != null)
+                .collect(Collectors.toMap(GenericEntity::getId, t -> t));
+
+        List<T> mergedList = new ArrayList<>();
+
+        for (T src : source) {
+            UUID id = src.getId();
+            if (id == null) {
+                newEntityInitializer.accept(src);
+                mergedList.add(src);
+            } else {
+                T tgt = targetMap.get(id);
+                if (tgt == null) {
+                    T managed = service.findById(id);
+                    copyProperties.accept(src, managed);
+                    mergedList.add(managed);
+                } else {
+                    copyProperties.accept(src, tgt);
+                    mergedList.add(tgt);
+                }
+            }
+        }
+        target.clear();
+        target.addAll(mergedList);
+    }
+
+    public static <T extends GenericEntity> void mergeListsSimple(
+            List<T> source,
+            List<T> target,
+            GenericCRUDService<T> service,
+            String... ignoreProperties) {
+
+        mergeLists(
+                source,
+                target,
+                service,
+                (src, tgt) -> BeanUtils.copyProperties(src, tgt, ignoreProperties),
+                t -> {
+                }
+        );
     }
 }
