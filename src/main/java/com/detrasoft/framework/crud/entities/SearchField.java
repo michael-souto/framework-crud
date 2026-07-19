9cdb8cbc899a4dabba67d33a01a370226168c745
Michael Souto <michael.souto@detrasoft.com>
2026-07-18 22:18:02 -0300

    feat: add principal and key fields and include header alias methods to SearchField entity

--- src/main/java/com/detrasoft/framework/crud/entities/SearchField.java
+++ src/main/java/com/detrasoft/framework/crud/entities/SearchField.java
@@ -10,13 +10,33 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class SearchField {
+
 	private String label;
 	private String field;
 	private String columnName;
 	private String where;
 	private boolean hidden;
+	private boolean principal;
+	private boolean key;
 
 	private String subfield;
 	private FieldType type;
 	private Object value;
+
+	public SearchField(String header, String field, String subfield, FieldType type, String columnName, String where) {
+		this.label = header;
+		this.field = field;
+		this.subfield = subfield;
+		this.type = type;
+		this.columnName = columnName;
+		this.where = where;
+	}
+
+	public String getHeader() {
+		return label;
+	}
+
+	public void setHeader(String header) {
+		this.label = header;
+	}
 }
